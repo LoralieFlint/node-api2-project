4ade7f8dfea5b9174e87858c3382e0c2a37bc6cf
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-12-07 12:39:54 -0700

    get post comments by id works

--- index.js
+++ index.js
@@ -38,25 +38,44 @@ server.get("/posts/:id", (req, res) => {
     });
 });
 
-server.post("/api/posts", (req, res) => {
-  const newPost = req.body
-  if (!newPost.title || !newPost.contents) {
-    res
-      .status(400)
-      .json({
-        errorMessage: "Please provide title and contents."
+server.get("/posts/:id/comments", (req, res) => {
+  const commentsById = req.params.id;
+  db.findPostComments(commentsById)
+    .then(post => {
+      if (commentsById.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+      if (post.length === 0) {
+        res.status(400).json({ errorMessage: "No comments here." });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the post"
       });
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents."
+    });
   } else {
     db.insert(newPost)
       .then(post => {
         res.status(201).json(post);
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({
-            error: "There was an error while saving the post to the database."
-          });
+        res.status(500).json({
+          error: "There was an error while saving the post to the database."
+        });
       });
   }
 });
